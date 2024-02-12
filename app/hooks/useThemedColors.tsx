ff4219ff688c12328128e008472ba980d9428b09
Salam Fazil <salaamfazil@icloud.com>
2024-02-11 16:23:37 -0800

    removed useless theme hook

--- app/hooks/useThemedColors.tsx
+++
@@ -1,9 +0,0 @@
-import { useColorScheme } from 'react-native';
-import { colors } from '../constants/styles/global';
-
-const useThemedColors = () => {
-    const colorScheme = useColorScheme();
-    return colors[colorScheme] || colors.light;
-};
-
-export default useThemedColors;
